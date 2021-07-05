30cdfd7c8c42a7511ab8ad4f02c30fdc3c91c80d
ssebadduka <seba.jamir@gmail.com>
2021-07-05 07:52:43 +0300

    gunicorn added

--- prayingapp/settings.py
+++ prayingapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zl8mcvp5i+!_9(qz@aob5b-t^k1205+r%l1zg2-@x6$)i_!+7b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['adhola.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
